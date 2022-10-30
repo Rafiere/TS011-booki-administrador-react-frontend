3d2c2e1dea7be3708895112067f338132d9669c7
Rafael <rafapgbr@gmail.com>
2022-10-30 09:15:25 -0300

    feat: inserção de rotas para as páginas da aplicação.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,15 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import { LoginPage } from "../pages";
+import { EsqueciASenha } from "../pages/EsqueciASenha/EsqueciASenha";
 
 export const AppRoutes = () => {
     return (
         <Routes>
-            <Route path="pagina-inicial" element={<LoginPage />} />
-            <Route path="livros" />
+            <Route path="/pagina-inicial" element={<LoginPage />} />
+            <Route path="/esqueci-a-senha" element={<EsqueciASenha />} />
+            <Route path="/livros" />
 
-            <Route path="*" element={<Navigate to="pagina-inicial>" />} />
+            <Route path="*" element={<Navigate to="pagina-inicial" />} />
         </Routes>
     );
 };
