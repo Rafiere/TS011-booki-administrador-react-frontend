ea844db48eb74b0c0021554f47a570272b9cb7b9
Rafael <rafapgbr@gmail.com>
2022-10-30 09:15:46 -0300

    feat: inserção de página para o login.

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,7 +1,44 @@
+import { Box, Center, Flex, HStack, Spacer, Text, VStack } from "@chakra-ui/react";
+import { addLocale, Button, Card, InputText, locale, Password } from "primereact";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import traducoes from "../../config/traducoes.json";
+
 export const LoginPage = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    useEffect(() => {
+        addLocale("pt", traducoes.pt);
+
+        locale("pt");
+        console.log("testando");
+    }, []);
+
     return (
-        <div>
-            <h1>Teste</h1>
-        </div>
+        <Center>
+            <Box maxW="1280px">
+                <Card>
+                    <Flex gap="4px" direction="column">
+                        <Center>
+                            <h2>Booki Administrador</h2>
+                        </Center>
+                        <Box>
+                            <h5>Email</h5>
+                            <InputText placeholder="Email" value={email} onChange={e => setEmail(e.target.value)} className="box" />
+                        </Box>
+                        <Spacer />
+                        <Box>
+                            <h5>Senha</h5>
+                            <Password placeholder="Senha" value={password} onChange={e => setPassword(e.target.value)} toggleMask />
+                        </Box>
+                        <Text>
+                            <Link to="/esqueci-a-senha"> Esqueci a senha </Link>
+                        </Text>
+                        <Button label="Logar" aria-label="Submit" className="p-button-md mt-2" />
+                    </Flex>
+                </Card>
+            </Box>
+        </Center>
     );
 };
