9404041f8680527c9ef0dfee5ba1292249050250
Rafael <rafapgbr@gmail.com>
2022-10-30 09:15:07 -0300

    feat: inserção das rotas na aplicação.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import { AppRoutes } from "./routes";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
     return (
-        <AppRoutes />
+        <BrowserRouter>
+            <AppRoutes />
+        </BrowserRouter>
         //A
     );
 }
