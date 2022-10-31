d1da32b54f131aa080da6784ff39600af03eeceb
Rafael <rafapgbr@gmail.com>
2022-10-30 21:23:08 -0300

    feat: adição do AuthProvider nas rotas.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { AppRoutes } from "./routes";
 import { BrowserRouter } from "react-router-dom";
+import { AuthProvider } from "./shared/contexts/AuthProvider";
 
 function App() {
     return (
-        <BrowserRouter>
-            <AppRoutes />
-        </BrowserRouter>
+        <AuthProvider>
+            <BrowserRouter>
+                <AppRoutes />
+            </BrowserRouter>
+        </AuthProvider>
         //A
     );
 }
