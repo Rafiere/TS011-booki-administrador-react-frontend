b2bd36035e229da15f8eb5f6eb1e3c71ca61347d
Rafael <rafapgbr@gmail.com>
2022-10-30 21:36:06 -0300

    fix: correção de import com erro.

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-const Dotenv = require("dotenv-webpack");
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [
-        react(),
-        new Dotenv({
-            systemvars: true,
-        }),
-    ],
+    plugins: [react()],
     server: {
         port: 3000,
     },
