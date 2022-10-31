729fa96efe3a530507ef7defa325f0852e5a018a
Rafael <rafapgbr@gmail.com>
2022-10-30 21:23:32 -0300

    feat: inserção de lógica para recuperar a senha.

--- src/pages/EsqueciASenha/EsqueciASenha.tsx
+++ src/pages/EsqueciASenha/EsqueciASenha.tsx
@@ -3,9 +3,24 @@ import { addLocale, Button, Card, InputText, locale } from "primereact";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import pt from "../../config/traducoes.json";
+import { addTranslationsToPtBrOnPage } from "../../config/traducoes";
+import { useAuth } from "../../shared/contexts/AuthProvider";
 
 export const EsqueciASenha = () => {
     const [email, setEmail] = useState("");
+
+    const { resetPassword } = useAuth();
+
+    addTranslationsToPtBrOnPage();
+
+    async function recoverPassword() {
+        try {
+            console.log("teste");
+            await resetPassword(email);
+        } catch (error) {
+            console.log(error);
+        }
+    }
 
     return (
         <Center>
@@ -17,7 +32,12 @@ export const EsqueciASenha = () => {
                     <h5>Digite o seu email</h5>
                     <InputText placeholder="Email" value={email} onChange={e => setEmail(e.target.value)} className="box" />
                     <Spacer />
-                    <Button label="Recuperar a Senha" aria-label="Recuperar a Senha" className="p-button-md mt-5" />
+                    <Button
+                        label="Recuperar a Senha"
+                        aria-label="Recuperar a Senha"
+                        className="p-button-md mt-5"
+                        onClick={recoverPassword}
+                    />
                     <Link to="/pagina-inicial" className="mt-5">
                         Voltar
                     </Link>
