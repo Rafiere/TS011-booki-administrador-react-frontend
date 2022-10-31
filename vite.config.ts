6c97e4f5506c6d58d11b96f302f3ac0813f5beee
Rafael <rafapgbr@gmail.com>
2022-10-30 21:34:14 -0300

    feat: inserção de configurações no projeto.

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+const Dotenv = require("dotenv-webpack");
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        react(),
+        new Dotenv({
+            systemvars: true,
+        }),
+    ],
     server: {
         port: 3000,
     },
